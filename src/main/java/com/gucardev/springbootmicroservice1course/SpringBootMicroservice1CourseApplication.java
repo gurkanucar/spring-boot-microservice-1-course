e841c330323ee6068c81f0d807bc0aff6cb1aebb
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 11:02:22 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/springbootmicroservice1course/SpringBootMicroservice1CourseApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootmicroservice1course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice1CourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroservice1CourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/springbootmicroservice1course/SpringBootMicroservice1CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootmicroservice1course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservice1CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
