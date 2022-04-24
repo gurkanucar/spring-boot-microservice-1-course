98fcad5b3a7821876c4d6a7d56a09022a168e374
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 11:26:32 +0300

    controller endpoint updated

--- src/main/java/com/gucardev/springbootmicroservice1course/controller/CourseController.java
+++ src/main/java/com/gucardev/springbootmicroservice1course/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/course")
+@RequestMapping("/api/course")
 public class CourseController {
 
     private final CourseService courseService;
